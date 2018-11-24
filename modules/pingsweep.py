8062df22cb525d477ea32d3e2fd547c1e2226706
Sidney G Dodson <siddodson@gmail.com>
2018-11-24 12:05:58 -1000

    Update

---
+++ modules/pingsweep.py
@@ -0,0 +1,5 @@
+def pingsweep(ip_addr):
+    print('Performing ping sweep with ttl {}'.format(ip_addr))
+
+def set_ttl(ttl):
+    print(ttl)
--- nsk.py
+++ nsk.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 import argparse
+# import modules.pingsweep as ps
+# import modules.traceroute as tr
+
+from modules import *
+from modules.pingsweep import pingsweep as ps, set_ttl
+import modules.traceroute as tr
 
 def pingsweep(args):
-    print(args)
+    ps(args.ip)
+    set_ttl(1200)
+    print(args.ip)
 
 def traceroute(args):
-    print(args)
+    tr.traceroute(args.target)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Network Survival Kit',
                                     description='Command line network toolkit')
 
     # parser.add_argument('ip', nargs=1, default='127.0.0.1', help='IP address')
-
     # parser.add_argument('--port', '-p', type=int)
-
     # parser.add_argument('--protocal', '-P', nargs='?', default='tcp', choices=['tcp', 'udp', 'icmp'])
 
     subparsers = parser.add_subparsers(help='Module specific utilities')
